05ec1158f2c39666b7638eab4d224417eccf3632
ambartri3 <32319229+ambartri3@users.noreply.github.com>
2017-11-06 10:58:36 +0800

    inputan inisialisasi yang akan dipilih

--- Luas Persegi dan segitiga.cpp
+++ Luas Persegi dan segitiga.cpp
@@ -9,6 +9,11 @@ int main() {
 	char INS;
 	int HS;
 	
-	
+	cout <<" Ketik P untuk menghitung persegi \n";
+		cout <<" Ketik S untuk menghitung Segitiga \n";
+		cout<<"\n";
+		cout<<"\n";
+		cout <<"TENTUKAN PILIHANMU SEKARANG       :";
+		cin >> INS;
 }
 
