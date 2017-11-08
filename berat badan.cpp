d1feb2612034c171bedc648539341f62c0fb2d45
ambartri3 <32319229+ambartri3@users.noreply.github.com>
2017-11-08 12:05:09 +0800

    memberi inputan

--- berat badan.cpp
+++ berat badan.cpp
@@ -7,6 +7,9 @@ int main()
     float bb;
     int tb;
     
-    
+    cout << "Silahkan masukkan tinggi badan(cm) : "; 
+	 cin >>tb;
+    cout << "Silahkan masukkan berat badan(kg) : " ;
+     cin >> bb;
 }
 
