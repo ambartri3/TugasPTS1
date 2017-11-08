883a14e19dcb66dcb6f692771708af5a374505ac
ambartri3 <32319229+ambartri3@users.noreply.github.com>
2017-11-08 19:37:07 +0800

    jika jam kerja lebih dari 40 jam menjalankan perintah berikut

--- gaji perminggu.cpp
+++ gaji perminggu.cpp
@@ -7,4 +7,13 @@ int main(){
 	cin>> jk;
 	cout<<"Gaji Perjam Karyawan   : "; 
 	cin>>gp;
+	
+if (jk>40){
+	le=jk-40;
+	jam=le*1.5;
+	ul=gp*1.5;
+	pa=ul*0.15;
+	gtot=gp-pa;
+	gb=gtot-(jk*40)+((gp-40)*jk*1.5);
+	}
 }
